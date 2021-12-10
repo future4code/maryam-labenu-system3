f31157d2f8af816a3b2d834e1f3d7eacc9138fc8
Emilly Santiago <emillysantiago89@gmail.com>
2021-12-10 18:19:05 -0300

    Endpoints dos estudantes

--- src/endpoints/createTeacher.ts
+++ src/endpoints/createTeacher.ts
@@ -47,24 +47,8 @@ export const createTeacher = async (req: Request, res: Response) => {
     const date = formatDate();
     data_nasc = formatDefaultDate(data_nasc);
 
-        await connection("maryam_docentes").insert({
-            id: Date.now().toString(),
-            name,
-            email,
-            data_nasc,
-            turma_id
-          });
-          res.status(200).send("Teacher criado");
-        
-    } catch (error: any) {
-        if(res.statusCode === 200){
-            res.status(500).send("Um erro inesperado ocorreu")
-        }else{
-            res.send(error.message)
-        }
-    }
 
-    if (formatDefaultDate(data_nasc).valueOf() < date.valueOf()) {
+   if (formatDefaultDate(data_nasc).valueOf() < date.valueOf()) {
       res.statusCode = 422;
       throw new Error(`A deadline must be later than the current date.`);
     }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-import app from "./app"
+import app from "./app";
 import { createEspecialidade } from "./endpoints/createEspecialidade";
 import { createStudent } from "./endpoints/createStudent";
+import { createStudentHobby } from "./endpoints/createStudentHobby";
 import { createTeacher } from "./endpoints/createTeacher";
 import { getAllEspecialidade } from "./endpoints/getAllEspecialidade";
-import { getAllStudents } from "./endpoints/getAllStudents";
+import { getStudentByName } from "./endpoints/getStudentByName";
 import { getAllTeacher } from "./endpoints/getAllTeacher";
-import { updateTeacher } from "./endpoints/updateTeacher";
+import { updateStudentClass } from "./endpoints/updateStudentClass";
+import {updateTeacher} from "./endpoints/updateTeacher"
 
 
-app.get("/students", getAllStudents);
+app.get("/student/:name", getStudentByName);
 app.post("/students", createStudent);
+app.put("/student/:id", updateStudentClass);
+app.post("/hobby", createStudentHobby);
+
 
 app.get("/teacher", getAllTeacher);
 app.post("/teacher", createTeacher);
-app.put("/teacher/:id", updateTeacher)
+app.put("/teacher/:id", updateTeacher);
 
-app.get("/especialidade", getAllEspecialidade)
-app.post("/especialidade", createEspecialidade)
+app.get("/especialidade", getAllEspecialidade);
+app.post("/especialidade", createEspecialidade);
+
